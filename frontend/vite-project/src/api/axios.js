d82c481c87e3ae8986ff8e23f6453a59ba1339ff
tarunpatidar2805 <tarunpatidarrupariya@gmail.com>
2026-03-30 00:06:22 +0530

    third

--- frontend/vite-project/src/api/axios.js
+++ frontend/vite-project/src/api/axios.js
@@ -1,7 +1,32 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     config.headers["Content-Type"] = "application/json";
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
 import axios from "axios";
 
+const rawBaseUrl = import.meta.env.VITE_API_BASE_URL || "";
+const normalizedBaseUrl = rawBaseUrl.replace(/\/$/, "");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: normalizedBaseUrl,
 });
 
 api.interceptors.request.use(
